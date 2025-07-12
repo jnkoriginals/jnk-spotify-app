61c6aaeb978184964460bbc5f54aaf373df66da1
jnkoriginals <jan.thieme@icloud.com>
2025-07-12 10:01:03 +0200

    test

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -10,6 +10,8 @@ export async function GET() {
         redirect_uri: process.env.SPOTIFY_REDIRECT_URI!,
     });
 
+    console.log(params);
+
     return NextResponse.redirect(
         `https://accounts.spotify.com/authorize?${params.toString()}`
     );
